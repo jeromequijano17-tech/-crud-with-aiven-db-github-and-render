ff456ec7015db6c21ba3e5e32a4e847ee54365d6
Jerome Quijano <jeromequijano17@gmail.com>
2026-04-13 23:22:19 +0800

    commit

--- server.js
+++ server.js
@@ -3,26 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// MySQL connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", // your MySQL password
-  database: "simpledb"
-});
-
-db.connect(err => {
-  if (err) {
-    console.log("DB connection failed:", err);
-  } else {
-    console.log("Connected to MySQL!");
-  }
-});
-
-// Home page (form + data)
+// Home page
 app.get("/", (req, res) => {
   res.send("Hello! Your Node.js app is running on Render 🚀");
 });
