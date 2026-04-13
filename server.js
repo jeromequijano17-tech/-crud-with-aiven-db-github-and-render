cae8cb6a2b1ec01ba9104279654a3db6a3177dfe
Jerome Quijano <jeromequijano17@gmail.com>
2026-04-13 23:10:28 +0800

    commit

--- server.js
+++ server.js
@@ -1,66 +1,11 @@
 const express = require("express");
-const mysql = require("mysql2");
-const bodyParser = require("body-parser");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// MySQL connection
-const db = mysql.createConnection({
-  host: "mysql-37610bae-jeromequijano17-df99.l.aivencloud.com",
-  user: "jerome",
-  password: "AVNS_-YAxsM4LYuR2M5SLjeq", // your MySQL password
-  database: "simpledb"
-  
-});
-
-db.connect(err => {
-  if (err) {
-    console.log("DB connection failed:", err);
-  } else {
-    console.log("Connected to MySQL!");
-  }
-});
-
-// Home page (form + data)
+// Home page
 app.get("/", (req, res) => {
-  db.query("SELECT * FROM messages", (err, results) => {
-    if (err) return res.send("Database error");
-
-    let html = `
-      <h1>Simple Node.js + MySQL Website</h1>
-
-      <form method="POST" action="/add">
-        <input type="text" name="message" placeholder="Enter message" required />
-        <button type="submit">Save</button>
-      </form>
-
-      <h2>Messages:</h2>
-      <ul>
-    `;
-
-    results.forEach(row => {
-      html += `<li>${row.message}</li>`;
-    });
-
-    html += `</ul>`;
-
-    res.send(html);
-  });
-});
-
-// Add message
-app.post("/add", (req, res) => {
-  const message = req.body.message;
-
-  db.query("INSERT INTO messages (message) VALUES (?)", [message], (err) => {
-    if (err) return res.send("Insert error");
-
-    res.redirect("/");
-  });
+  res.send("Hello! Your Node.js app is running on Render 🚀");
 });
 
 // Start server
